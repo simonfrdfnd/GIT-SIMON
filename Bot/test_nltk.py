492fa354fddf650dcc17796cb5d417829b249041
simonfrdfnd <simon.frdfnd@gmail.com>
2024-01-20 22:26:53 +0100

    Bot database yournexio

--- Bot/test_nltk.py
+++ Bot/test_nltk.py
@@ -30,11 +30,11 @@ print(f"Time taken to fetch incidents from the database: {end_time - start_time}
 start_time = time.time()
 
 # Extraire les descriptions de problème de chaque ticket depuis la base de données
-existing_tickets = [(row[1], row[2]) for row in rows]  # Utiliser à la fois la short description et la description complète
+existing_tickets = [row[1] for row in rows]  # Utiliser à la fois la short description et la description complète
 
 # Prétraitement des données existantes
-def preprocess_text(short_description, description):
-    text = f"{short_description} {description}".lower()  # Combinaison de la short description et de la description
+def preprocess_text(short_description):
+    text = f"{short_description}".lower()  # Combinaison de la short description et de la description
     words = word_tokenize(text)
     stop_words = set(stopwords.words('english'))
     words = [word for word in words if word not in stop_words]
@@ -42,14 +42,14 @@ def preprocess_text(short_description, description):
     words = [stemmer.stem(word) for word in words]
     return ' '.join(words)
 
-preprocessed_tickets = [preprocess_text(short_description, description) for short_description, description in existing_tickets]
+preprocessed_tickets = [preprocess_text(short_description) for short_description in existing_tickets]
 
 # Nouveau ticket (à remplacer par la saisie de l'utilisateur)
 new_ticket_short_description = "le parametre du measurement time n'est pas envoyé correctement à l'ESW8 (EMI test receiver)"
 new_ticket_description = "Suite à la derniere mise à jour de BAT, il y a un nouveau probleme avec le driver de l'ESW8 qui est apparu. Le mesurement time est systematiquement réglé à 1ms sur l'EMI test receiver quelque soit la valeur indiquée dans BAT. Le probleme apparait quelque soit le mode de l'EMI (time domain ou linear scan). En configurant l ESW avec le driver de l ESR, on s apercoit que le probleme est résolu. Mais c etait juste pour identifier que le probleme vient bien du driver ESW.dll Pouvez-vous rapidement fixer ce probleme ?"
 
 # Prétraitement du nouveau ticket
-preprocessed_new_ticket = preprocess_text(new_ticket_short_description, new_ticket_description)
+preprocessed_new_ticket = preprocess_text(new_ticket_short_description)
 
 # Utilisation de TF-IDF pour la représentation vectorielle des tickets
 vectorizer = TfidfVectorizer()
@@ -62,6 +62,9 @@ cosine_similarities = cosine_similarity(tfidf_matrix[-1], tfidf_matrix[:-1]).fla
 similarities_with_indices = [(i, similarity) for i, similarity in enumerate(cosine_similarities)]
 sorted_similarities = sorted(similarities_with_indices, key=lambda x: x[1], reverse=True)
 
+end_time = time.time()
+print(f"Time taken to analyzer the incidents: {end_time - start_time} seconds")
+
 # Nombre de top tickets similaires à afficher
 top_x = 20
 
@@ -70,3 +73,4 @@ print(f"Top {top_x} similarités avec les tickets existants:")
 for i in range(min(top_x, len(sorted_similarities))):
     index, similarity = sorted_similarities[i]
     print(f"Similarity with existing ticket {rows[index][0]}: {similarity}")
+
--- Bot/test_transformers.py
+++ Bot/test_transformers.py
@@ -1,30 +1,13 @@
 from transformers import pipeline
-import fitz  # PyMuPDF
-
-def extract_text_from_pdf(pdf_file_path):
-    doc = fitz.open(pdf_file_path)
-
-    text = ""
-    for page_number in range(doc.page_count):
-        page = doc[page_number]
-        text += page.get_text()
-
-    doc.close()
-
-    return text
 
 if __name__ == "__main__":
-    pdf_file_path = "test1.pdf"
-
-    extracted_text = extract_text_from_pdf(pdf_file_path)
-
     # Chargez le modèle de question-réponse pré-entraîné BERT
     question_answering = pipeline('question-answering', model='bert-base-uncased', tokenizer='bert-base-uncased')
 
-    documentation = extracted_text
+    documentation = "The measured disturbance level is used to control the generator level and to reach the target. The disturbance level must be measured."
 
     # Posez une question à l'IA
-    question = "What is the transfer function of a JIG?"
+    question = "What is the closed loop method?"
 
     # Obtenez la réponse
     answer = question_answering(question=question, context=documentation)

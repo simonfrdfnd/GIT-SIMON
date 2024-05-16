022e041b502f9f37579f1579180c03ed5e06cd51
simonfrdfnd <simon.frdfnd@gmail.com>
2024-05-16 10:21:38 +0200

    Création de la nouvelle vue

---
+++ NexioMax3/NexioMax3.Definition/View/NexioMax3View.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace NexioMax3.Definition.View
+{
+  using System.Windows.Controls;
+
+  /// <summary>
+  /// Interaction logic for NexioMax3View.xaml
+  /// </summary>
+  public partial class NexioMax3View : UserControl
+  {
+    public NexioMax3View()
+    {
+      this.InitializeComponent();
+    }
+  }
+}
---
+++ NexioMax3/NexioMax3.Standalone/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace NexioMax3.Standalone
+{
+  using NexioMax3.Definition.ViewModel;
+
+  /// <summary>
+  /// Logique d'interaction pour MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      this.InitializeComponent();
+      var vm = new EmiOverrideViewModel();
+      vm.CloseRequested += this.VmOnCloseRequested;
+      this.DataContext = vm;
+    }
+
+    private void VmOnCloseRequested(object sender, RoutedEventArgs e)
+    {
+      this.Close();
+    }
+  }
+}
